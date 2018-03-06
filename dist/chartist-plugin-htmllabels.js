c0bb42adbd504185ecba9eea9afeb12f98d21950
Sebastian Hilger <hi@7talents.de>
2018-03-06 09:49:18 +0100

    forgot to grunt in 0.2.0

--- dist/chartist-plugin-htmllabels.js
+++ dist/chartist-plugin-htmllabels.js
@@ -1,42 +1,43 @@
 (function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(["chartist"], function (Chartist) {
-      return (root.returnExportsGlobal = factory(Chartist));
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["chartist"], function (a0) {
+      return (root['Chartist.plugins.ctHtmlLabels'] = factory(a0));
     });
-  } else if (typeof exports === 'object') {
+  } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like enviroments that support module.exports,
+    // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("chartist"));
   } else {
-    root['Chartist.plugins.ctHtmlLabels'] = factory(Chartist);
+    root['Chartist.plugins.ctHtmlLabels'] = factory(root["Chartist"]);
   }
 }(this, function (Chartist) {
 
-  /**
-   * Chartist.js plugin to provide the ability to use HTML labels again.
-   *
-   */
-  /* global Chartist */
-  (function(window, document, Chartist) {
-      'use strict';
+/**
+ * Chartist.js plugin to provide the ability to use HTML labels again.
+ *
+ */
+/* global Chartist */
+(function(window, document, Chartist) {
+    'use strict';
 
-      Chartist.plugins = Chartist.plugins || {};
-      Chartist.plugins.ctHtmlLabels = function() {
+    Chartist.plugins = Chartist.plugins || {};
+    Chartist.plugins.ctHtmlLabels = function() {
 
-          return function ctHtmlLabels(chart) {
-              chart.on('draw', function(context) {
-                  if (context.type === 'label') {
-                      // Best to combine with something like sanitize-html
-                      context.element.empty()._node.innerHTML = context.text;
-                  }
-              });
-          };
-      };
+        return function ctHtmlLabels(chart) {
+            chart.on('draw', function(context) {
+                if (context.type === 'label') {
+                    // Best to combine with something like sanitize-html
+                    context.element.empty()._node.innerHTML = context.text;
+                }
+            });
+        };
+    };
 
-  }(window, document, Chartist));
+}(window, document, Chartist));
 
-  return Chartist.plugins.ctHtmlLabels;
+return Chartist.plugins.ctHtmlLabels;
 
 }));
